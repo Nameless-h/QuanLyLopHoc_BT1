034cb9e24e5b3dcba3b21f5fac08331aec992739
2NgonNgon2 <tranvihao40@gmail.com>
2024-02-04 20:56:15 +0700

    first commit

---
+++ src/main/java/BUS/PersonBUS.java
@@ -0,0 +1,14 @@
+package BUS;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Vi Hao
+ */
+public class PersonBUS {
+    
+}
