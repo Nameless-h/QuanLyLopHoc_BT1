9a9fd9771e54585b40cfe54dffa6801971ff44ba
Huchuynh <117436192+Huchuynh@users.noreply.github.com>
2024-02-20 11:43:57 +0700

    Update AddInstructorForm.java

--- src/main/java/GUI/CourseInstructor/AddInstructorForm.java
+++ src/main/java/GUI/CourseInstructor/AddInstructorForm.java
@@ -58,15 +58,6 @@ public class AddInstructorForm extends javax.swing.JFrame {
         this.parent = (MainForm) mafr;
     }
 
-    public AddInstructorForm(MainForm mafr) {
-        initComponents();
-        setVisible(true);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        personBUS.LoadPersonToComboBox(cbPerson);
-        departmentBUS.LoadCourseToComboBox(cbDepartment);
-        this.parent = (MainForm) mafr;
-    }
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
