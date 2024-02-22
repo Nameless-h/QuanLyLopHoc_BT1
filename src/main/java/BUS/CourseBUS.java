640cf6778402c1668d8822b6f5293f1840189f49
ankoa <100978052+ankoa@users.noreply.github.com>
2024-02-22 12:13:21 +0700

    Update CourseBUS.java

--- src/main/java/BUS/CourseBUS.java
+++ src/main/java/BUS/CourseBUS.java
@@ -56,6 +56,8 @@ public class CourseBUS {
         }
         return false;
     }
-    
+    public boolean validateData(CourseDTO c) {
+        return !c.getCredits().isEmpty() && !c.getTitle().isEmpty();
+    }
     
 }
